b1c818f2fa566d3c0f25b3b185cf7341f2abc7c0
vash2pid <vashdread@gmail.com>
2024-10-07 09:30:19 +0800

    Add project files.

---
+++ PakCacher.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace pakcacher
+{
+    internal class PakCacher
+    {
+        struct ZipFileInfo
+        {
+            public int signature;
+            public short version;
+            public short flag;
+            public short compressmethod;
+            public short modtime;
+            public short moddate;
+            public int crc;
+            public int compressed;
+            public int uncompressed;
+            public short filenamelen;
+            public short extrafieldlen;
+            public char[] filename;
+            public int offset;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No file provided. Drag and drop *.pak or *.zip file(s) in the executable");
+                Console.WriteLine("Press any key to close window...");
+                Console.ReadKey();
+                return;
+            }
+
+            int ctr = 1;
+            foreach (string filepath in args)
+            {
+                Console.WriteLine("Processing file {0} of {1}", ctr, args.Length);
+                if (File.Exists(filepath)
+                    & (".pak".Equals(Path.GetExtension(filepath)) | ".zip".Equals(Path.GetExtension(filepath))
+                    )
+                )
+                {
+                    Console.WriteLine("Input: " + filepath);
+                    try
+                    {
+                        bool isPak = ".pak".Equals(Path.GetExtension(filepath));
+
+                        CheckSignature(filepath);
+
+                        using (FileStream fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+                        {
+                            string outputfilename = (isPak) ? fileStream.Name + ".cache" : fileStream.Name.Replace(Path.GetExtension(filepath), "") + ".header";
+
+                            Console.WriteLine("Output: " + outputfilename);
+
+                            if (File.Exists(outputfilename))
+                                File.Move(outputfilename, outputfilename + "." + DateTime.Now.ToString("yyyyMMddHHmmssffff"));
+
+
+                            using (BinaryReader reader = new BinaryReader(fileStream))
+                            using (FileStream writestream = new FileStream(outputfilename, FileMode.Create, FileAccess.ReadWrite))
+                            {
+                                if (!isPak)
+                                {
+                                    writestream.Write(new byte[] { 0x51, 0x34, 0x00, 0x00 }, 0, 4);
+                                }
+
+                                ZipFileInfo zipFileInfo;
+
+                                zipFileInfo.signature = reader.ReadInt32();
+                                zipFileInfo.offset = 0;
+
+                                while (zipFileInfo.signature.Equals(67324752))
+                                {
+
+                                    zipFileInfo.version = reader.ReadInt16();
+                                    zipFileInfo.flag = reader.ReadInt16();
+                                    zipFileInfo.compressmethod = reader.ReadInt16();
+                                    zipFileInfo.modtime = reader.ReadInt16();
+                                    zipFileInfo.moddate = reader.ReadInt16();
+                                    zipFileInfo.crc = reader.ReadInt32();
+                                    zipFileInfo.compressed = reader.ReadInt32();
+                                    zipFileInfo.uncompressed = reader.ReadInt32();
+                                    zipFileInfo.filenamelen = reader.ReadInt16();
+                                    zipFileInfo.extrafieldlen = reader.ReadInt16();
+                                    zipFileInfo.filename = reader.ReadChars(zipFileInfo.filenamelen);
+
+                                    // to data offset
+                                    zipFileInfo.offset += 30 + zipFileInfo.filenamelen;
+
+                                    //skip file data
+                                    reader.ReadBytes(zipFileInfo.compressed);
+
+                                    if (isPak) CreateCache(zipFileInfo, writestream);
+                                    else CreateHeader(zipFileInfo, writestream);
+
+                                    //next file offset
+                                    zipFileInfo.offset += zipFileInfo.compressed;
+
+                                    // advance to next file sig
+                                    zipFileInfo.signature = reader.ReadInt32();
+                                }
+                            }
+
+                            Console.WriteLine("File {0}: {1} completed\n", ctr, outputfilename);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message.ToString() + "\n");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a *.pak or a *.zip\n", Path.GetFileName(filepath));
+                }
+                ctr++;
+            }
+
+            Console.WriteLine("Completed processing {0} file(s)", args.Length);
+            Console.WriteLine("Press any key to close window...");
+            Console.ReadKey();
+        }
+
+        private static void CheckSignature(string filePath)
+        {
+            using (BinaryReader reader = new BinaryReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
+            {
+                if (!reader.ReadInt32().Equals(67324752))
+                    throw new Exception("The file " + filePath + " header did not start with the expected signature. Make sure the file is correct.");
+            }
+        }
+
+        private static void CreateCache(ZipFileInfo zipFileInfo, FileStream writestream)
+        {
+            byte[] toWrite = BitConverter.GetBytes((Int32)6)
+                .Concat(new byte[] { 0x3F })
+                .Concat(BitConverter.GetBytes((Int32)zipFileInfo.filenamelen))
+                .Concat(System.Text.Encoding.UTF8.GetBytes(zipFileInfo.filename))
+                .Concat(BitConverter.GetBytes((Int64)zipFileInfo.offset))
+                .Concat(BitConverter.GetBytes((Int64)zipFileInfo.uncompressed))
+                .Concat(BitConverter.GetBytes((Int64)zipFileInfo.compressed))
+                .Concat(BitConverter.GetBytes(zipFileInfo.compressmethod))
+                .Concat(BitConverter.GetBytes(zipFileInfo.crc))
+                .ToArray();
+            writestream.Write(toWrite, 0, toWrite.Length);
+        }
+
+        private static void CreateHeader(ZipFileInfo zipFileInfo, FileStream writestream)
+        {
+
+            byte[] toWrite = BitConverter.GetBytes((Int32)zipFileInfo.filenamelen)
+                .Concat(System.Text.Encoding.UTF8.GetBytes(zipFileInfo.filename))
+                .Concat(BitConverter.GetBytes((Int64)zipFileInfo.offset))
+                .Concat(BitConverter.GetBytes((Int64)zipFileInfo.uncompressed))
+                .ToArray();
+            writestream.Write(toWrite, 0, toWrite.Length);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+﻿Introduction
+------------
+The software is used to create a *.cache file for Warhammer 40,000: Space Marine 2 *.pak files.
+It can also create a *.header file for zipped audio files.
+
+This is achived by reading each file's header information in the acrhive file and recreating the .cache/.header file base on thar information.
+
+Requirements
+------------
+Requires .Net Framework 4.8
+
+Usage
+-----
+Drag and drop *.pak or *.zip file(s) on the executable for it to create corresponing *.cache or *.header file.
+The created file will be in the same directory of the dropped file. 
+If a *.cache or *.header file already exists for the file, the existing is backed up before the new one is created. 
+
+Disclaimer
+----------
+Although this tool does not modify the *.pak or *.header files, and creates backup of the existing *.cache file.
+It's till recommened to backup your original files before using the tool.
+
+If the original files are lost or unrecoveralbe in any way, it's advisable to verity integrity of files on Steam or reinstall the game.
+
+This tool does not bypass any anti-cheat mechanism of the game and does not guarantee that the recreated *.cache
+will not trigger an AVF error.
+
+
+Known Issues
+------------
+Recreating the *.cache file for default_animation.pak and default_video_5.pak files does work correctlty with the current version. 
+It will trigger a crash on start up, cache file for default_animation.pak and default_video_5.pak seems to be different than the other pak files.
+
+
